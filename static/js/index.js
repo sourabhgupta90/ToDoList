93fc950204a4f5efa99f858f2385786ffddf908f
Sourabh Gupta <sourabh.gupta@hashedin.com>
2012-12-29 00:40:22 +0530

    border correction

--- static/js/index.js
+++ static/js/index.js
@@ -1,18 +1,16 @@
 $("#add-new").click(function(){
-	
-	$($(".span3.employee-list")[0]).clone().insertAfter($(".span3.employee-list")[3]);
+
+$($(".span3.employee-list")[0]).clone().insertBefore($(".span3.employee-list")[3]);
 
 
 });
 
 /*
 $(document).ready(function(){
-	  $('.goodbye').hide();
-	  
-	  $("#btn1").click(function(){
-		  $($('.goodbye')[0]).clone().show().appendTo('.hello');
-	  });
-	  
-	});
+$('.goodbye').hide();
+$("#btn1").click(function(){
+$($('.goodbye')[0]).clone().show().appendTo('.hello');
+});
+});
 
 */
